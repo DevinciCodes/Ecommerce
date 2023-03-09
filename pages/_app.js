5281a1226327e96d73bcdc48d8da5013ac26e958
Devin Latham <devinlatham20@gmail.com>
2023-03-09 16:06:00 -0600

    Create basic template for design with header, main card component, and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import React from 'react'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import{ Layout } from '../components'
+
+ function App({ Component, pageProps }) {
+
+  return (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+  )
 }
+
+export default App
